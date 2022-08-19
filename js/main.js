7ae9bf79eee660c4de0315aab0eb970a4613c920
George <gbasulto2015@gmail.com>
2022-08-19 14:11:11 -0400

    Working on languajes

--- js/main.js
+++ js/main.js
@@ -18,6 +18,157 @@
 })(document);
 
 ((d)=>{
+
+    const $menuLinks = d.querySelectorAll(".menu a"),
+          $heroTitle = d.querySelector(".hero-image-title"),
+          $btnContact = d.querySelector(".btn-contact"),
+          $btnCV = d.querySelector(".btn-cv"),
+          $aboutExperience = d.querySelector(".about p"),
+          $skillTitle = d.querySelector(".skills h2"),
+          $skills = d.querySelectorAll(".skills .progress .skill"),
+          $servicesTitle = d.querySelector(".services h2"),
+          $servicesName = d.querySelectorAll(".services .service-card h3"),
+          $servicesDescription = d.querySelectorAll(".services .service-card p"),
+          $works = d.querySelector(".portfolio h2"),
+          $worksTitle = d.querySelectorAll(".portfolio .portfolio-card h3"),
+          $worksDescription = d.querySelectorAll(".portfolio .portfolio-card p"),
+          $portfolioTitle = d.querySelectorAll(".portfolio-info h3"),
+          $portfolioDescription = d.querySelectorAll(".portfolio-info p")
+        
+        
+
+
+
+    const languaje = {
+        en:{
+            menuHome: "Home",
+            menuAbout: "About",
+            menuServices: "Services",
+            menuPorfolio: "Portfolio",
+            menuTestimonials: "Testimonial",
+            menuContact: "Contact",
+            heroTitle: "Welcome!",
+            heroContact: "CONTACT ME",
+            aboutBtn:"Download my CV",
+            aboutExperience:"Since 2004 I have experience in the area of web development, network administration and SQL databases for tourism companies and educational institutions. Also collaborated on small projects as a full stack freelance although I am more oriented to the front-end area. I enjoy making the layout and interactivity of web interfaces",
+            skillText:"Skills",
+            skillFront: "FrontEnd Development",
+            skillDesign:"Web Design",
+            skillBackend:"BackEnd Web Development",
+            servicesTitle:"Services",
+            webDev:"Web Development",
+            webDesign:"Web Design",
+            webHost:"Web Hosting",
+            devDescription:"I develop sites and applications for your web platforms I help you build your project from start to finish front-end backend and database",
+            designDescription:"I prototype and mock up your static designs and turn them into amazing responsive, optimal and accessible web sites and applications",
+            hostDescription:"You do not have a domain and web hosting for your project?Don't worry about those technical things, I'll take care of them",
+            works:"My works",
+            workCreditScoring:"Credit Score GTM",
+            workWilfredo:"Portfolio of Dr. Wilfredo Campos from the University of Guantánamo",
+            workUniversityGtm:"Agriculture Campus University of Guantanamo",
+            workECommerce:"GB Store",
+            workSpa:"SPA App with Vanilla JS",
+            workGeorgeB:"Portfolio Ing. George Basulto",
+            creditScoreDescrp:"Credit scoring model to improve the granting of microcredits",
+            wilfredoDescrp:"Web site with information about Dr Wilfredo Campos, his works and skills.",
+            universityGtmDescrp:"Website with informaction of the Faculty of Agriculture of the University of Guantánamo",
+            ecommerceDescrp:"Electronic commerce for medical equipment sales",
+            spaDescrp:"SPA aplication using JS vanilla, fetching data from WordPress API",
+            georgeBDescrp:"Portfolio Ing. George L Basulto Reyes, whit his works and skills.",
+        },
+        es:{
+            menuHome: "Inicio",
+            menuAbout: "Acerca",
+            menuServices: "Servicios",
+            menuPorfolio: "Portafolio",
+            menuTestimonials: "Testimonio",
+            menuContact: "Contacto",
+            heroTitle:"Bienvenidos!",
+            heroContact:"CONTÁCTAME",
+            aboutBtn:"Descarga mi CV",
+            aboutExperience:" Desde 2004 tengo experiencia en el área del desarrollo web, administracion de redes y bases de datos SQL para compañias de turismo e instituciones educacionales. He colaborado en pequeños proyectos como full stack aunque estoy mas orientado al area de FontEnd. Disfruto llevar a cabo la maquetación e interactividad de interfaces web.",
+            skillText:"Habilidades",
+            skillFront: "Desarrollo Web Front End",
+            skillDesign:"Diseño Web",
+            skillBackend:"Desarrollo Web Back End",
+            servicesTitle:"Servicios",
+            webDev:"Desarrollo Web",
+            webDesign:"Diseño Web",
+            webHost:"Alojamiento Web",
+            devDescription:"Desarrollo sitios y aplicaciones para plataformas Web. Te ayudo a construir tu proyecto de principio a fin: Frontend, Backend y Base de Datos.",
+            designDescription:"Prototipo y maqueto tus diseños estáticos y los convierto en increíbles sitios y aplicaciones web responsivas, óptimas y accesibles. ",
+            hostDescription:"¿No cuentas con dominio y hospedaje web para tu proyecto? No te preocupes de esas cosas técnicas me encargo yo.",
+            works:"Mis trabajos",
+            workCreditScoring:"Credit Score GTM",
+            workWilfredo:"Portafolio Wilfredo Campos",
+            workUniversityGtm:"Facultada de Agricultura de la Universidad de Guantanamo",
+            workECommerce:"Ecommerce GBStore",
+            workSpa:"Aplicacion SPA con vanilla JS",
+            workGeorgeB:"Portafolio Ing. George Basulto",
+            creditScoreDescrp:"Modelo de credito para perfeccionar la concesion de microcreditos",
+            wilfredoDescrp:"Portafolio del Doctor Wilfredo Campos de la Universidad e Guantanamo con informacion acerca de su trabajo y habilidades",
+            universityGtmDescrp:"Sitio Web con informacion de la facultad de agricultura de la Universidad de Gtmo",
+            ecommerceDescrp:"Comercio Electronico para la venta de equipamiento medico",
+            spaDescrp:"Aplicacion SPA usando solo Java Script y consumiendo la API de WordPress",
+            georgeBDescrp:"Portafolio del Ing. George L Basulto Reyes, en donde se pueden observar sus trabajos mas recientes asi como sus habilidades.",
+        }
+    }
+
+    const changeLanguaje = (lang)=>{
+          for (let menuLinks of $menuLinks) {
+           menuLinks.textContent = lang[menuLinks.dataset.menu];
+                     
+          }
+          $heroTitle.textContent = lang.heroTitle;
+          $btnContact.textContent = lang.heroContact;
+          $btnCV.textContent = lang.aboutBtn;
+          $aboutExperience.textContent =lang.aboutExperience;
+          $skillTitle.textContent = lang.skillText;
+          $servicesTitle.textContent =lang.servicesTitle;
+          $works.textContent = lang.works;
+
+          for (let skill of $skills) {
+            skill.textContent = lang[skill.dataset.skill]
+          } 
+          for (let serviceName of $servicesName) {
+            serviceName.textContent = lang[serviceName.dataset.service]
+          } 
+          for (let serviceDescription of $servicesDescription) {
+            serviceDescription.textContent = lang[serviceDescription.dataset.description]
+          } 
+          for (let workTitle of $worksTitle) {
+            workTitle.textContent = lang[workTitle.dataset.work]
+          } 
+          for (let workDescription of $worksDescription) {
+            workDescription.textContent = lang[workDescription.dataset.description]
+          } 
+          for (let portfolioTitle of $portfolioTitle) {
+            portfolioTitle.textContent = lang[portfolioTitle.dataset.title]
+          } 
+          for (let portfolioDescription of $portfolioDescription) {
+            portfolioDescription.textContent = lang[portfolioDescription.dataset.description]
+          } 
+
+
+
+
+    }
+            
+         d.addEventListener("click", e=>{
+             const {es, en} = languaje;
+             if(e.target.matches(".languaje__en")) {
+                  changeLanguaje(en)
+           
+             }
+             if(e.target.matches(".languaje__es")){
+                changeLanguaje(es)
+           
+             }
+              
+          })
+ })(document);
+
+((d)=>{
       
     d.addEventListener("click", e=>{
         // console.log(e.target)
